19d80f6fc45807ebfeb3ba60afcedcffad07ae61
abhishekverma108 <71845709+abhishekverma108@users.noreply.github.com>
2023-06-01 12:05:10 +0000

    change_done_jsonify

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 app=Flask(__name__)
+JOBS=[
+  {
+    "id" :1,
+    "title":"data analyst",
+    "location": "banglore",
+    "salary": "Rs 5,00,000"
+  },
+  {
+    "id" :2,
+    "title":"data scientist",
+    "location": "Delhi,India",
+    "salary": "Rs 8,00,000"
+  },
+  {
+    "id" :3,
+    "title":"sde",
+    "location": "pune",
+    "salary": "Rs 9,00,000"
+  },
+  {
+    "id" :4,
+    "title":"frontend",
+    "location": "remote",
+    
+  }
+]
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 
